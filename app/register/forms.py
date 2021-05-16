43e7d404148ec66cef2325cb0d677895a6140a07
ShuangQuan Zheng <shuangquanzheng@ShuangQuans-MBP.lan>
2021-05-16 23:31:36 +0800

    Completed the login and regesiter page and changed nav a bit so items are together Changed and added placeholder for login and changed name/label of regster for correction

--- app/register/forms.py
+++ app/register/forms.py
@@ -2,15 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, length, EqualTo
 
+
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    firstname = StringField('Firstname', validators=[DataRequired()])
-    lastname = StringField('Lastname', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(),length(min=6, max=80)])
-    confirmpassword = PasswordField('Confirm Password', validators=[DataRequired(),length(min=6, max=80), EqualTo('password')])
-    email = StringField("email", validators=[DataRequired(),Email()])
+    username = StringField('User Name', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), length(min=6, max=80)])
+    confirmpassword = PasswordField('Confirm Password', validators=[
+                                    DataRequired(), length(min=6, max=80), EqualTo('password')])
+    email = StringField("Email", validators=[DataRequired(), Email()])
     submit = SubmitField('Sign up')
-    
+
     def username_validate(self, username):
         usr = users.query.filter_by(username=username.data).first()
         if user is not None:
@@ -20,7 +23,8 @@ class RegisterForm(FlaskForm):
         usr = users.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please enter a different email address.')
-    
+
     def password_validate(self, password):
         if len(password) < 6:
-            raise ValidationError("Password has to be atleast 6 characters long")
+            raise ValidationError(
+                "Password has to be atleast 6 characters long")
